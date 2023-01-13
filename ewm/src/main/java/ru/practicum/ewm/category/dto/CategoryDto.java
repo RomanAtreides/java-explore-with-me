f86595f9e718b88a810a08c69dcac4750b483399
Roman Nazarenko <justaspeckinspace@gmail.com>
2023-01-13 13:11:41 +0300

    refactor: change pom files
    and remove unused import from CategoryDto class

--- ewm/src/main/java/ru/practicum/ewm/category/dto/CategoryDto.java
+++ ewm/src/main/java/ru/practicum/ewm/category/dto/CategoryDto.java
@@ -3,7 +3,6 @@ package ru.practicum.ewm.category.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.practicum.ewm.utility.marker.Create;
 import ru.practicum.ewm.utility.marker.Update;
 
 import javax.validation.constraints.NotNull;
